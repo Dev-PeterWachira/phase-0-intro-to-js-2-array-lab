8b8181190cdba53753d3c74f17a5a75b7cd1644a
Dev-PeterWachira <wmainap@gmail.com>
2024-10-09 14:46:25 +0300

    array methods

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+let cats = ['Milo','Otis','Garfield'];
+function destructivelyAppendCat(name){
+cats.push('Ralph');
+}
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat(){
+cats.splice(-1);
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+   return[...cats,'Broom'];
+}
+function prependCat(name){
+    return['Arnold',...cats];
+}
+function removeLastCat(){
+    return cats.toSpliced(-1,1);
+}
+function removeFirstCat(){
+   return cats.toSpliced(0,1);
+}
